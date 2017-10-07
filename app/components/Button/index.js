bc5bbdd1f9f8d634b5f6565ec13a83d272609609
Nathan Hood <nathanhood@nathans-mbp.attlocal.net>
2017-10-07 16:54:01 -0500

    Create Button component for testing styling configuration

---
+++ app/components/Button/index.js
@@ -0,0 +1,28 @@
+/**
+*
+* Button
+*
+*/
+
+import React from 'react';
+// import styled from 'styled-components';
+
+import { FormattedMessage } from 'react-intl';
+import messages from './messages';
+import { button } from './style.scss';
+
+class Button extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+    render() {
+        return (
+            <button className={button}>
+                <FormattedMessage {...messages.header} />
+            </button>
+        );
+    }
+}
+
+Button.propTypes = {
+
+};
+
+export default Button;
--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -11,14 +11,18 @@
 
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
+import Button from 'components/Button';
 import messages from './messages';
 
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
-  render() {
-    return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h1>
+                    <FormattedMessage {...messages.header} />
+                </h1>
+                <Button />
+            </div>
+        );
+    }
 }
