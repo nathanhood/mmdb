539265d40572a19684dbcc705ceda38b087ffe55
Nathan Hood <nathanhood@nathans-mbp.attlocal.net>
2017-10-07 17:12:11 -0500

    Convert indentation in app directory

--- app/containers/App/selectors.js
+++ app/containers/App/selectors.js
@@ -3,10 +3,10 @@ import { createSelector } from 'reselect';
 const selectRoute = (state) => state.get('route');
 
 const makeSelectLocation = () => createSelector(
-  selectRoute,
-  (routeState) => routeState.get('location').toJS()
+    selectRoute,
+    (routeState) => routeState.get('location').toJS()
 );
 
 export {
-  makeSelectLocation,
+    makeSelectLocation,
 };
