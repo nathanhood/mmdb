2bf2c94dde4ab7ba28753a45b944dc0440d45700
Nathan Hood <nathanhood@me.com>
2017-10-15 16:21:10 -0500

    Clean up express server configuration
    - Remove prod middleware and include in index.js
    to make easier to reason about
    - Remove unused code from boilerplate

--- server/index.js
+++ server/index.js
@@ -4,57 +4,48 @@ const express = require('express');
 const logger = require('./logger');
 const argv = require('./argv');
 const port = require('./port');
-// const setup = require('./middlewares/frontendMiddleware');
 const isDev = process.env.NODE_ENV !== 'production';
 const ngrok = (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel ? require('ngrok') : false;
 const resolve = require('path').resolve;
-// const app = express();
-// const appPath = resolve(process.cwd(), 'app');
-// const App = require(`${appPath}/containers/Homepage`);
-// const fs = require('fs');
-// var { renderToString } = require('react-dom/server');
-// const webpackConfig = require('../internals/webpack/webpack.server.babel');
-// const webpack = require('webpack');
-
-// import logger from './logger';
-// import argv from './argv';
-// import port from './port';
-// import { resolve } from 'path';
-// import express from 'express';
-// import fs from 'fs';
-// import handleRender from '../build/server.js';
+const compression = require('compression');
 const handleRender = require('./build/server.js');
 const app = express();
 
-// If you need a backend, e.g. an API, add your custom backend-specific middleware here
+/**
+ * API endpoints
+ */
 // app.use('/api', myApi);
 
-// In production we need to pass these values in instead of relying on webpack
-// setup(app, {
-//     outputPath: resolve(process.cwd(), 'build'),
-//     publicPath: '/',
-// });
-// Add webpack dev server middleware for hot reloading
+/**
+ * Dev Mode middleware for hot reloading
+ *
+ * NOTE: Nothing below this block will be evaluated when in dev mode
+ */
 if (isDev) {
     const webpackConfig = require('../internals/webpack/webpack.dev.babel'); // eslint-disable-line global-require
     const addDevMiddlewares = require('./middlewares/addDevMiddlewares'); // eslint-disable-line global-require
     addDevMiddlewares(app, webpackConfig);
 }
 
-// Serve static files
+// Compression middleware compresses your server responses which makes them
+// smaller (applies also to assets). You can read more about that technique
+// and other good practices on official Express.js docs http://mxs.is/googmy
+app.use(compression());
+
+// Serve static files from 'build' directory
 app.use('/', express.static(resolve(process.cwd(), 'build')));
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
     // TODO: Create store
     res.send(handleRender.default());
 });
 
-// get the intended host and port number, use localhost and port 3000 if not provided
+// Get the intended host and port number, use localhost and port 3000 if not provided
 const customHost = argv.host || process.env.HOST;
 const host = customHost || null; // Let http.Server use its default IPv6/4 host
 const prettyHost = customHost || 'localhost';
 
-// Start your app.
+// Start your app
 app.listen(port, host, (err) => {
     if (err) {
         return logger.error(err.message);
