c5f6b5efa50d29403104fc9d05692565eb3e72da
Peter Bedor <peterbedor@gmail.com>
2017-10-19 23:44:41 -0500

    Add icon component

---
+++ app/components/Icon/index.js
@@ -0,0 +1,24 @@
+/**
+*
+* Icon
+*
+*/
+
+import React from 'react';
+// import styled from 'styled-components';
+
+
+class Icon extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    }
+}
+
+Icon.propTypes = {
+
+};
+
+export default Icon;
