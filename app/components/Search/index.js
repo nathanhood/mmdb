1c48fde0651035e03b63b54e59d96f663f689cb7
Peter Bedor <peterbedor@gmail.com>
2017-10-21 13:53:30 -0500

    Add Search component

---
+++ app/components/Search/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// import styled from 'styled-components';
+
+
+class Search extends React.PureComponent {
+    render() {
+        return (
+            <div>
+                hey
+            </div>
+        );
+    }
+}
+
+Search.propTypes = {
+
+};
+
+export default Search;
