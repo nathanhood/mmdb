bf77c0e4b0f3900b5c1078924bb1c5a3d16854fc
Nathan Hood <nathanhood@me.com>
2017-11-04 22:56:02 -0500

    Create initial auth middleware

---
+++ server/middlewares/auth.js
@@ -0,0 +1,15 @@
+const db = require('../models');
+
+module.exports = app => {
+    return app.use('/', (req, res, next) => {
+        // TODO: Retrieve user id from request
+        db.User.findById(1).then(user => {
+            if (user) {
+                req.user = user;
+                next();
+            } else {
+                res.send(403);
+            }
+        })
+    });
+};
