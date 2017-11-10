ea2543ce53602e2ea1066bf6ee02163572dcb05d
Peter Bedor <peterbedor@gmail.com>
2017-11-09 19:45:27 -0600

    Clean up seeder files and fix table names

--- server/seeders/20171104223305-genresSeeder.js
+++ server/seeders/20171104223305-genresSeeder.js
@@ -1,93 +1,91 @@
 const genres = [
     {
-        "tmdbId": 28,
-        "name": "Action",
+        tmdbId: 28,
+        name: 'Action',
     },
     {
-        "tmdbId": 12,
-        "name": "Adventure",
+        tmdbId: 12,
+        name: 'Adventure',
     },
     {
-        "tmdbId": 16,
-        "name": "Animation",
+        tmdbId: 16,
+        name: 'Animation',
     },
     {
-        "tmdbId": 35,
-        "name": "Comedy",
+        tmdbId: 35,
+        name: 'Comedy',
     },
     {
-        "tmdbId": 80,
-        "name": "Crime",
+        tmdbId: 80,
+        name: 'Crime',
     },
     {
-        "tmdbId": 99,
-        "name": "Documentary",
+        tmdbId: 99,
+        name: 'Documentary',
     },
     {
-        "tmdbId": 18,
-        "name": "Drama",
+        tmdbId: 18,
+        name: 'Drama',
     },
     {
-        "tmdbId": 10751,
-        "name": "Family",
+        tmdbId: 10751,
+        name: 'Family',
     },
     {
-        "tmdbId": 14,
-        "name": "Fantasy",
+        tmdbId: 14,
+        name: 'Fantasy',
     },
     {
-        "tmdbId": 36,
-        "name": "History",
+        tmdbId: 36,
+        name: 'History',
     },
     {
-        "tmdbId": 27,
-        "name": "Horror",
+        tmdbId: 27,
+        name: 'Horror',
     },
     {
-        "tmdbId": 10402,
-        "name": "Music",
+        tmdbId: 10402,
+        name: 'Music',
     },
     {
-        "tmdbId": 9648,
-        "name": "Mystery",
+        tmdbId: 9648,
+        name: 'Mystery',
     },
     {
-        "tmdbId": 10749,
-        "name": "Romance",
+        tmdbId: 10749,
+        name: 'Romance',
     },
     {
-        "tmdbId": 878,
-        "name": "Science Fiction",
+        tmdbId: 878,
+        name: 'Science Fiction',
     },
     {
-        "tmdbId": 10770,
-        "name": "TV Movie",
+        tmdbId: 10770,
+        name: 'TV Movie',
     },
     {
-        "tmdbId": 53,
-        "name": "Thriller",
+        tmdbId: 53,
+        name: 'Thriller',
     },
     {
-        "tmdbId": 10752,
-        "name": "War",
+        tmdbId: 10752,
+        name: 'War',
     },
     {
-        "tmdbId": 37,
-        "name": "Western",
+        tmdbId: 37,
+        name: 'Western',
     }
-]
+];
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('genres', genres.map(genre => ({
-        ...genre,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        slug: genre.name.toLowerCase().replace(' ', '-'),
-    })), {});
-  },
+    up: (queryInterface) => {
+        return queryInterface.bulkInsert('Genres', genres.map(genre => ({
+            ...genre,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+            slug: genre.name.toLowerCase().replace(' ', '-'),
+        })), {});
+    },
 
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('genres', null, {});
-  }
+    down: (queryInterface) => queryInterface.bulkDelete('Genres', null, {})
 };
--- server/seeders/20171104224217-usersSeeder.js
+++ server/seeders/20171104224217-usersSeeder.js
@@ -1,18 +1,16 @@
-'use strict';
-
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-      return queryInterface.bulkInsert('users', [{
-        firstName: 'Nathan',
-        lastName: 'Hood',
-        email: 'nathanhood@me.com',
-        password: 'password',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }], {});
-  },
+    up: (queryInterface) => {
+        return queryInterface.bulkInsert('Users', [{
+            firstName: 'Nathan',
+            lastName: 'Hood',
+            email: 'nathanhood@me.com',
+            password: 'password',
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        }], {});
+    },
 
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('users', null, {});
-  }
+    down: (queryInterface) => {
+        return queryInterface.bulkDelete('Users', null, {});
+    }
 };
