76b7ac41c71024eb4ca2603a07134f4c455d9c69
Nathan Hood <nathanhood@me.com>
2017-11-13 20:42:24 -0600

    Add box shadow to movie card component

--- app/components/LibraryCard/index.js
+++ app/components/LibraryCard/index.js
@@ -6,6 +6,7 @@ import theme from 'theme';
 
 const StyledContainer = styled.div`
     background: ${theme.white};
+    box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.06);
     border-radius: 2px;
     margin-bottom: 25px;
     padding: 15px;
