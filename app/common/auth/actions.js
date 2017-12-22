0b19f71288564629ff75bc33772cd19ca8024727
Peter Bedor <peterbedor@gmail.com>
2017-12-22 13:45:37 -0600

    Add registration actions

--- app/common/auth/actions.js
+++ app/common/auth/actions.js
@@ -1,5 +1,7 @@
 export const AUTH_SUCCESSFUL = 'auth/AUTH_SUCCESSFUL';
 export const AUTH_FAILURE = 'auth/AUTH_FAILURE';
+export const REGISTER_SUCCESSFUL = 'auth/REGISTER_SUCCESSFUL';
+export const REGISTER_FAILURE = 'auth/REGISTER_FAILURE';
 
 export const authSuccess = (user) => ({
     type: AUTH_SUCCESSFUL,
@@ -8,4 +10,12 @@ export const authSuccess = (user) => ({
 
 export const authFailure = () => ({
     type: AUTH_FAILURE,
+});
+
+export const registerSuccess = () => ({
+    type: REGISTER_SUCCESSFUL,
+});
+
+export const registerFailure = () => ({
+    type: REGISTER_FAILURE,
 });
