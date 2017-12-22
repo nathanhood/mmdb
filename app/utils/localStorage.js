21e763fbaa18518a260a7d1d4eb67b9be58f59bf
Nathan Hood <nathanhood@me.com>
2017-12-22 16:53:55 -0600

    Accommodate use of localStorage in server environment
    - Webpack provides `process.browser` to safely exclude code for builds intended for a Node environment

--- app/utils/localStorage.js
+++ app/utils/localStorage.js
@@ -1,15 +1,26 @@
-const get = (key, isObject = true) => {
-    let data = localStorage.getItem(key);
+let store = () => undefined;
+let get = () => null;
 
-    if (data && isObject) {
-        data = JSON.parse(data);
-    }
+if (process.browser) {
+    get = (key, isObject = true) => {
+        let data = localStorage.getItem(key);
 
-    return data;
-};
+        if (data && isObject) {
+            data = JSON.parse(data);
+        }
+
+        return data;
+    };
+
+    store = (key, data, isObject = true) => {
+        if (isObject) {
+            data = JSON.stringify(data);
+        }
+
+        localStorage.setItem(key, data);
+    };
+}
 
 export const getUser = () => get('user');
 
-export const storeUser = (user) => {
-    localStorage.setItem('user', JSON.stringify(user));
-};
+export const storeUser = (user) => store('user', user);
