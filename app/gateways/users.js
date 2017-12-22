4c2a4ed30e69b8c6912d2b76c57e6e77aa5683c7
Peter Bedor <peterbedor@gmail.com>
2017-12-22 13:50:02 -0600

    Add register user function to users gateway

--- app/gateways/users.js
+++ app/gateways/users.js
@@ -5,3 +5,8 @@ import { extractDataFromResponse } from './utils';
 export const loginUser = ({ username, password }) => {
     return fetch.post('/login', { username, password }).then(extractDataFromResponse);
 };
+
+export const registerUser = ({ email, username, password, passwordConfirmation }) => {
+    return fetch.post('/register', { email, username, password, passwordConfirmation })
+        .then(extractDataFromResponse);
+}
