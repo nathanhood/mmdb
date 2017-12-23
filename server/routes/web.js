cc6437af3838d981ee99535a2251dee63d2efd19
Nathan Hood <nathanhood@me.com>
2017-12-23 11:43:46 -0600

    Update production server to return HTML skeleton on all routes
    This will allow our app to run in a production environment until we get SSR in a more steady place

--- server/routes/web.js
+++ server/routes/web.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const dashboardController = require('../controllers/pages/dashboardController');
 
-router.get('/', dashboardController.get);
+router.get('*', dashboardController.get);
 
 module.exports = router;
